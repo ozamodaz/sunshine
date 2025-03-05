b2ba46b24f02f7f6321f296d3a9f4e6770d3efd2
Oleh Horen <oleg.goren@competera.net>
2025-03-05 17:52:04 +0200

    upd

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # sunshine
+
+This is my set of tools for managing growbox with Raspberry and Python.
+
+The most interesting part in my opinion is solution for fan speed management.
+I failed with dimmer-moniles and ended with set of capacitors connected thru
+relays and in parallel, so I can change Capacity turning on and off some
+capacitors. I made some tests and chosen handy nominals of capacitors to
+have near 10 steps between fully off and bypass. Keep in mind that lowest
+speed fan can handle can be lower than the lowest speed it can start.
+
+# Configuration:
+
+Set time of sunrise and lenght of daytime in `conf.ini` 
+
+Set fan speed like this:
+
+```
+echo '4' > /opt/fan_speed_day
+echo '2' > /opt/fan_speed_night
+```
+(this can be changed without service restart or reload.
+Changes in ini-file will require restart)
--- sunshine.py
+++ sunshine.py
@@ -1,58 +1,74 @@
 import configparser
-from pyA20.gpio import gpio
-from pyA20.gpio import port
+import logging
+import RPi.GPIO as GPIO
 from datetime import datetime
 from datetime import timedelta
+from datetime import time
 from time import sleep
-import os
 import sys
-import signal
 
-lamp = port.PA7
-fan = port.PA8
-fullspeed = port.PA9
+logging.basicConfig(level=logging.INFO)
+GPIO.setmode(GPIO.BCM)
 
-day =   [
-        (lamp, 1),
-        (fan, 1),
-        (fullspeed, 1)
-        ]
+# init Capacitors for fan speed control
+cap_2 = 17
+cap_1a = 27
+cap_1b = 22
+cap_1c = 23
+cap_05 = 24
+bypass = 25
+GPIO.setup(cap_2,  GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(cap_1a, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(cap_1b, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(cap_1c, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(cap_05, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(bypass, GPIO.OUT, initial=GPIO.HIGH)
 
-night = [
-        (lamp, 0),
-        (fan, 1)
-        ]
+# Init misc (additional) channels
+misc_1 = 5
+misc_2 = 6
+GPIO.setup(misc_1, GPIO.OUT, initial=GPIO.HIGH) # Misc 1
+GPIO.setup(misc_2, GPIO.OUT, initial=GPIO.HIGH) # Misc 2
 
-silent = [
-        (fan, 1),
-        (fullspeed, 0)
-        ]
+# Lamp
+lamp = 16
+GPIO.setup(lamp, GPIO.OUT, initial=GPIO.LOW) # Lamp
+logging.info('GPIO init complete')
 
+# Read config
 config = configparser.ConfigParser()
-config.read(os.path.join(os.path.dirname(__file__), 'conf.ini'))
-DATE_FORMAT = '%H:%M'
+config.read(sys.argv[1])
 sunrise = config['Default']['sunrise']
-sunrise = datetime.strptime(sunrise, DATE_FORMAT)
+sunrise = datetime.strptime(sunrise, '%H:%M')
 t = datetime.now() - timedelta(days=1)
 sunrise = datetime(t.year, t.month, t.day, sunrise.hour, sunrise.minute)
+logging.info('Config read complete')
+logging.info('sunrise = %s' % sunrise)
 
+def set_fan_speed(level):
+    # Levels 0 to 10
+    # 0 is Off
+    # 10 is bypass
+    
+    ch_available = [cap_2, cap_1a, cap_1b, cap_1c, cap_05, bypass]
+    fan_speed_levels = [ [],
+                         [cap_1a],
+                         [cap_1a, cap_05],
+                         [cap_2],
+                         [cap_2, cap_05],
+                         [cap_2, cap_1a],
+                         [cap_2, cap_1a, cap_05],
+                         [cap_2, cap_1a, cap_1b],
+                         [cap_2, cap_1a, cap_1b, cap_05],
+                         [cap_2, cap_1a, cap_1b, cap_1c],
+                         [bypass]
+                        ]
 
-class GracefulKiller:
-    kill_now = False
+    for channel in set(ch_available) - set(fan_speed_levels[level]):
+        GPIO.output(channel, GPIO.HIGH)
 
-    def __init__(self):
-        signal.signal(signal.SIGINT, self.exit_gracefully)
-        signal.signal(signal.SIGTERM, self.exit_gracefully)
-
-    def exit_gracefully(self, signum, frame):
-        self.kill_now = True
-
-
-def set_mode(mode):
-    gpio.init()
-    for param in mode:
-        gpio.setcfg(*param)
-
+    for channel in fan_speed_levels[level]:
+        GPIO.output(channel, GPIO.LOW)
 
 def main():
     now = datetime.now()
@@ -61,26 +77,39 @@ def main():
     daytime = timedelta(hours=int(daytime))
     sunset = sunrise + daytime
     night_len = timedelta(hours=24) - daytime
+    logging.info('daytime = %s ' % daytime)
+    logging.info('sunset = %s ' % sunset)
+    logging.info('night_len = %s ' % night_len)
 
     if now > sunrise and now < sunset:
-        set_mode(day)
+        logging.info('It is Day now')
+        GPIO.output(lamp, GPIO.HIGH)
+        with open('/opt/fan_speed_day') as f:
+            fan_speed_day = int(f.read())
+        set_fan_speed(fan_speed_day)
+        logging.info('fan_speed = %s' % fan_speed_day)
 
     elif now > sunset and now > sunrise:
-        set_mode(night)
-        sleep(120)
-        set_mode(silent)
+        logging.info('It is Night now')
+        GPIO.output(lamp, GPIO.LOW)
+        with open('/opt/fan_speed_night') as f:
+            fan_speed_night = int(f.read())
+        set_fan_speed(fan_speed_night)
+        logging.info('fan_speed = %s' % fan_speed_night)
         sunrise = sunset + night_len
+        logging.info('sunrise = %s' % sunrise)
 
+    sleep(15)
 
 if __name__ == '__main__':
-    killer = GracefulKiller()
     while True:
         main()
-        if killer.kill_now:
-            break
-        sleep(5)
 
-sleep(5)
-set_mode(night)
-set_mode(silent)
+logging.info('Stopping...')
+logging.info('fan_speed = %s' % fan_speed_night)
+GPIO.output(lamp, GPIO.LOW)
+with open('/opt/fan_speed_night') as f:
+    fan_speed_night = int(f.read())
+set_fan_speed(fan_speed_night)
+sunrise = sunset + night_len
 sys.exit(0)
